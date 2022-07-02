54858866ffa3e0e53310eb4336185f73332f5d99
Renato Zanella <renatozr07@gmail.com>
2022-07-02 17:33:22 -0300

    add: controller create task

--- backend/src/controllers/task.js
+++ backend/src/controllers/task.js
@@ -6,6 +6,15 @@ const getAll = async (_req, res) => {
   res.status(200).json(tasks);
 };
 
+const create = async (req, res) => {
+  const { name, status } = req.body;
+
+  const task = await taskService.create(name, status);
+
+  res.status(201).json(task);
+};
+
 module.exports = {
   getAll,
+  create,
 };
