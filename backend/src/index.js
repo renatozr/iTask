cda22ac48a96554dd092b98c6d061b1dcee81952
Renato Zanella <renatozr07@gmail.com>
2022-07-02 16:58:21 -0300

    add: usa as rotas no servidor

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 require('dotenv/config');
+require('express-async-errors');
 const express = require('express');
 const cors = require('cors');
+const routes = require('./routes/index');
 
 const app = express();
 
@@ -8,6 +10,8 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (_req, res) => res.status(200).json({ message: 'OK!' }));
+
+app.use(routes);
 
 const PORT = process.env.PORT || 3001;
 
