29b9ec6247ee8fea2d0b35b1c0cdab5bab2216f4
Renato Zanella <renatozr07@gmail.com>
2022-07-03 20:53:09 -0300

    add: base frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TaskList from './components/TaskList';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>iTask</h1>
+        <TaskList />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/TaskList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function TaskList() {
+  return (
+    <div className="TaskList">
+      <ul className="list">
+        <li>Task Example</li>
+      </ul>
+    </div>
+  );
+}
+
+export default TaskList;
