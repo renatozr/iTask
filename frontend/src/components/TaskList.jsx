0874386d83f97509e0510a6b72f77740d7605bfc
Renato Zanella <renatozr07@gmail.com>
2022-07-04 10:37:21 -0300

    feat: connecta com a api buscando as tasks e renderizando-as

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,23 +1,18 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
-import Loading from './Loading';
-
 function TaskList() {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    axios.get('localhost:3001/tasks')
-      .then((data) => data.json())
-      .then((data) => setTasks(data));
+    axios.get('http://localhost:3001/tasks')
+      .then(({ data }) => setTasks(data));
   }, []);
 
   return (
     <div className="TaskList">
       <ul className="list">
-        {
-          tasks.length === 0 ? <Loading /> : tasks.map((task) => <li>{ task }</li>)
-        }
+        { tasks.map((task) => <li key={task.id}>{ `${task.name} - ${task.status}` }</li>) }
       </ul>
     </div>
   );
