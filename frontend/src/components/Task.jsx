60856bd9f1d4760e8ece5823165ea8812762ad7b
Renato Zanella <renatozr07@gmail.com>
2022-07-04 15:20:13 -0300

    feat: botão que exclui a task

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+
+const excludeTask = (taskId) => axios.delete(`http://localhost:3001/tasks/${taskId}`);
 
 function Task({ task }) {
+  const [isTaskExcluded, setIsTaskExcluded] = useState(false);
+
+  const handleExcludeBtn = () => {
+    setIsTaskExcluded(true);
+    excludeTask(task.id);
+  };
+
+  if (isTaskExcluded) return undefined;
+
   return (
-    <div>{ `${task.name} - ${task.status}` }</div>
+    <div>
+      <span>{ `${task.name} - ${task.status}` }</span>
+      <button type="button" onClick={handleExcludeBtn}>X</button>
+    </div>
   );
 }
 
 Task.propTypes = {
   task: PropTypes.shape({
+    id: PropTypes.number,
     name: PropTypes.string,
     status: PropTypes.string,
   }).isRequired,
