2f7f19a62c5c7473e20eca4a9b3711d57f6e802a
Renato Zanella <renatozr07@gmail.com>
2022-07-04 10:15:49 -0300

    add: componentes Loading e TaskList

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+import Loading from './Loading';
 
 function TaskList() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    axios.get('localhost:3001/tasks')
+      .then((data) => data.json())
+      .then((data) => setTasks(data));
+  }, []);
+
   return (
     <div className="TaskList">
       <ul className="list">
-        <li>Task Example</li>
+        {
+          tasks.length === 0 ? <Loading /> : tasks.map((task) => <li>{ task }</li>)
+        }
       </ul>
     </div>
   );
